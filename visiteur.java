c64fe9aaeceb7a6bca2a5a3d14d016d2aa691fe7
assietoudiagne <44315729+assietoudiagne@users.noreply.github.com>
2018-12-22 17:21:10 +0000

    Update visiteur.java

--- visiteur.java
+++ visiteur.java
@@ -4,6 +4,10 @@ public class Visiteur {
  
 	String ipAdress;
 	
+	public visiteur(String ipAdress){
+		this.ipAdress=ipAdress;
+	}
+	
 	public static void main(String[] args) {
 				Visiteur v = new Visiteur("125.2.2.87"); 
 		System.out.println(v.getIpAdress()); 	
