966125f17721fe1f596d7ae2d27f797a84de7a21
assietoudiagne <44315729+assietoudiagne@users.noreply.github.com>
2018-12-24 01:29:09 +0000

    Create ClientTest.java

---
+++ ClientTest.java
@@ -0,0 +1,29 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorImplTest {
+
+	@Test
+	public final void testMultiply() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDivide() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSubstract() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
